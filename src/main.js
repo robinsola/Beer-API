bcf499e39dbac12feb1426630d6bf7cedc33894e
Robin Sola and semhar kidane <robin@>
2018-04-26 10:09:33 -0700

    separate api logic from front-end file.

--- src/main.js
+++ src/main.js
@@ -3,37 +3,25 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import {API} from './API.js';
 
 $(document).ready(function() {
   $("#user-input").click(function(event){
     event.preventDefault();
-    // debugger;
     let name = $("#name-input").val();
     // let ingredient = $("#ingredient-input").val();
     // let fermentation = $("#fermentation-input").val();
 
-    let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = `https://api.punkapi.com/v2/beers?brewed_before=11-2012&abv_gt=6`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
-
+    let api = new API();
+    let promise = api.makeCall();
     promise.then(function(response) {
-      const body = JSON.parse(response);
-      $('#showname').text(`${body[0].name}`);
+      let body = JSON.parse(response);
+      $('#showname').text(name);
+      // $('#showname').text(`${body[0].name}`);
       $('.showingredient').text(`The ingredients are ${body[0].ingredients.malt[0].name}.`);
       $('.showfermentation').text(`Fermentation process is ${body[0].method.fermentation.temp.value} degrees ${body[0].method.fermentation.temp.unit}`);
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
   });
-
 });
